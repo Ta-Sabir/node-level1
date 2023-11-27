5e104908011f4ba7b7bdbbdbee0b422cb76b5c60
saber <talhaoui.sabir@gmail.com>
2023-11-27 10:47:20 +0100

    lesson3

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3001;
+const mongoose = require("mongoose");
+app.get("/", (req, res) => {
+  res.sendFile("./src/home.html", { root: __dirname });
+});
 
-app.get('/', (req, res) => {
-  res.sendFile("./src/home.html",{root: __dirname})
-})
-
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect(
+    "mongodb+srv://talhaouisabir:SvkcvBVUOhhnOGeY@cluster0.89ftj68.mongodb.net/all-data?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
